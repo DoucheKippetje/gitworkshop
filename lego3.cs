6b6ad4b1a84cb9afdff64bc4d4be419177c4ee1c
RedBrick2010 <rood.blokje.2010@gmail.com>
2021-09-28 10:53:09 +0200

    LEGO!

---
+++ lego3.cs
@@ -0,0 +1,14 @@
+// lego 3
+public class hallo {
+	private void WriteAstring(string h_string) {
+		Console.WriteLine(h_string);
+	}
+
+	private string DevString(){
+		return "Hello Developers!";
+	}	
+	
+	public void main() {
+		WriteAString(DevString());
+	}	
+}
