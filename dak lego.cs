d4596bd647ac912c447c571b9f4654e2138a846e
RedBrick2010 <rood.blokje.2010@gmail.com>
2021-09-28 11:13:50 +0200

    dakje toegevoegd

---
+++ dak lego.cs
@@ -0,0 +1,19 @@
+// lego 3
+
+
+public class hallo {
+	private void WriteAstring(string h_string) {
+		Console.WriteLine(h_string);
+	}
+
+	private string DevString(){
+		return "Hello Developers!";
+	}	
+	
+	public void main() {
+		WriteAString(DevString());
+	}	
+	private void lego() {
+		int x = 1;
+	}
+}
